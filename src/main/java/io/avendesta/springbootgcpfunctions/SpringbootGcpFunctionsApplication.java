e29aeedea61d05e48dd785cdf00ac2d1a6408670
aven <aven@Ubuntu.attlocal.net>
2023-12-25 01:57:17 -0600

    initialize basic springboot project

---
+++ src/main/java/io/avendesta/springbootgcpfunctions/SpringbootGcpFunctionsApplication.java
@@ -0,0 +1,13 @@
+package io.avendesta.springbootgcpfunctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGcpFunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGcpFunctionsApplication.class, args);
+	}
+
+}
